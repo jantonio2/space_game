5569dae2cb32fba378a11b605f6e909d659869e3
jantonio2 <jesusrochavedia@gmail.com>
2020-06-13 21:13:22 -0400

    Inicio del Proyecto

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:spacegame/User/bloc/bloc_user.dart';
+import 'package:spacegame/space_menu.dart';
+
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(
+          statusBarColor: Colors.transparent,
+          statusBarBrightness: Brightness.light
+      )
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        home: SpaceMenu()
+      ),
+      bloc: UserBloc(),
+    );
+  }
+}
---
+++ lib/space_menu.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:spacegame/main_game.dart';
+import 'package:spacegame/Score/ui/main_score.dart';
+import 'package:spacegame/User/ui/main_user.dart';
+
+class SpaceMenu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      bottomNavigationBar: CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+            activeColor: Colors.indigo,
+            backgroundColor: Color(0x33FFFFFF),
+            inactiveColor: Colors.white30,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.gamepad),
+                  title: Text("")
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.table_chart),
+                  title: Text("")
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person),
+                  title: Text("")
+              ),
+            ]
+        ),
+
+        tabBuilder: (BuildContext context, int index) {
+          switch (index) {
+            case 0:
+              return CupertinoTabView(
+                builder: (BuildContext context) => GameMain(),
+              );
+              break;
+            case 1:
+              return CupertinoTabView(
+                builder: (BuildContext context) => ScoreMain(),
+              );
+              break;
+            case 2:
+              return CupertinoTabView(
+                builder: (BuildContext context) => UserMain(),
+              );
+              break;
+
+          }
+
+        },
+      ),
+    );
+  }
+
+}
