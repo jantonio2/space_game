792440638130b2896c309edc89a9c79db55dae86
jantonio2 <jesusrochavedia@gmail.com>
2020-06-17 22:23:14 -0400

    Errores carajo

--- lib/User/repository/firebaseauthapi.dart
+++ lib/User/repository/firebaseauthapi.dart
@@ -10,7 +10,7 @@ class FirebaseAuthAPI {
     GoogleSignInAuthentication gSA = await googleSignInAccount.authentication;
 
     FirebaseUser user = (await _auth.signInWithCredential(
-        GoogleAuthProvider.getCredential(idToken: gSA.idToken, accessToken: gSA.accessToken)))  ;
+        GoogleAuthProvider.getCredential(idToken: gSA.idToken, accessToken: gSA.accessToken))) as FirebaseUser;
     /*final FirebaseAuth _auth=FirebaseAuth.instance;
     final GoogleSignIn googleSignIn= new GoogleSignIn();
     GoogleSignInAccount acount=await googleSignIn.signIn();
